10392dbcf13a8da927f0543dc9a9c0157e1f472d
Batuhantrkgl <batuhanturkoglu37@gmail.com>
2026-09-12 14:56:49 +0300

    j6lte: add fingerprint hidl and vendor packages

--- device.mk
+++ device.mk
@@ -66,7 +66,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.samsung
+    android.hardware.biometrics.fingerprint@2.1-service.samsung \
+    android.hardware.biometrics.fingerprint@2.1.vendor
 
 # NFC
 PRODUCT_PACKAGES += \
